67e1698500a8f110d3de3c2d4a11ac61e397df4c
sushmitha711 <sushmithan012@gmail.com>
2022-10-17 00:42:43 +0530

    Polce department of Berlin

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import {BrowserRouter as Router,Routes,Route} from 'react-router-dom';
+import Bikes from "./Components/BikeData/Bikes";
+import Navbar from "./Navbar/Navbar";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+let App=()=>{
+
+    return <Router>
+        <Navbar/>
+        <Routes>
+            <Route exact path="/" element={<Bikes/>}/>
+                    
+        </Routes>
+    </Router>
 }
-
-export default App;
+export default App
